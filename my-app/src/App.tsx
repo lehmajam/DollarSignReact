8a1280f693f78110450787f806016aa41cbc2fc9
stuckben <stuckben@students.zhaw.ch>
2022-02-21 19:51:56 +0100

    hello world change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hel
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          Hello world
         </p>
         <a
           className="App-link"
