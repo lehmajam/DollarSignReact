53c1f5cfca3e5f4b715e938ae26dab836865dc1b
stuckben <stuckben@students.zhaw.ch>
2022-02-22 21:09:05 +0100

    test commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo Velo
+          Hallo whatdupaffd
         </p>
         <a
           className="App-link"
