72bd13cd77c2a2222332fa6eec4fde8dc1d1db17
lehmajam <lehmajam@students.zhaw.ch>
2022-02-28 13:57:20 +0100

    whaaaaaaaaaaaa

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo whatdupaffd
+          Hallo whaaaaaaaaaaaaaaaaaaaaaaaa
         </p>
         <a
           className="App-link"
